6bd0c9f2547b27b8a63008b6e7966f21988ce8f8
Shubham Vishwakarma <144842142+shubhamvish765@users.noreply.github.com>
2024-04-25 23:55:40 +0530

    Add files via upload
    Basic to Advanced question of loop using c programming.

---
+++ Assignment3.c
@@ -0,0 +1,235 @@
+#include<stdio.h>
+
+// int main()
+// {
+//     int n;
+//     printf("Enter number: ");
+//     scanf("%d",&n);
+//     if(n>0)
+//     {
+//         printf("Positive");
+//     }
+//     else{
+//         printf("non-positive");
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n;
+//     printf("Enter number: ");
+//     scanf("%d",&n);
+//     if(n%5==0){
+//     printf("divisible by 5");
+//     }
+//     else{
+//     printf("not divisiable by 5");
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int year;
+//     printf("Enter year: ");
+//     scanf("%d",&year);
+//     if(year%4==0)
+//     printf("leap year");
+//     else
+//     printf("not leap year");
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     int a,b,c;
+//     printf("Enter 3 number: ");
+//     scanf("%d",&a);
+//     scanf("%d",&b);
+//     scanf("%d",&c);
+
+//     if(a>b && a>c)
+//     printf("%d is greatest",a);
+//     else if(b>a && b>c)
+//     printf("%d is greatest",b);
+//     else
+//     printf("%d is greatest",c);
+// }
+
+// int main()
+// {
+//    int CP,SP;
+//    printf("Enter CP and SP: ");
+//    scanf("%d %d",&CP,&SP);
+//    if(SP>CP)
+//    {
+//     int profit = SP-CP;
+//     int profit_per = (profit*100)/CP;
+//     printf("%d is profit",profit_per);
+//    }
+//    else if(SP<CP)
+//    {
+//     int loss = CP-SP;
+//     int loss_per = (loss*100)/CP;
+//     printf("%d is loss",loss_per);
+//    }
+//     return 0;
+// }
+
+// int main()
+// {
+//     char ch;
+//     printf("Enter alaphabet: ");
+//     scanf("%c",&ch);
+
+//     if(ch>='A' && ch<='Z')
+//     {
+//         printf("Uppercase");
+//     }
+//     else if(ch>='a' && ch<='z')
+//     {
+//         printf("lowercase");
+//     }
+//     else{
+//         printf("Special Character");
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int month;
+//     printf("Enter month from 1 to 12: ");
+//     scanf("%d",&month);
+
+//     if(month==1)
+//     printf("31 days");
+    
+//     else if(month==2)
+//     printf("28 days");
+
+//     else if(month==3)
+//     printf("30 days");
+
+//     else if(month==4)
+//     printf("31 days");
+
+//     else if(month==5)
+//     printf("30 days");
+
+//     else if(month==6)
+//     printf("31 days");
+
+//     else if(month==7)
+//     printf("30 days");
+
+//     else if(month==8)
+//     printf("31 days");
+
+//     else if(month==9)
+//     printf("30 days");
+
+//     else if(month==10)
+//     printf("31 days");
+
+//     else if(month==11)
+//     printf("30 days");
+
+//     else if(month==12)
+//     printf("31 days");
+    
+//     return 0;
+// }
+
+/*
+Write a program to take marks of 5 subjects from the user. 
+Assume marks are given 
+out of 100 and 
+passing marks is 33. 
+Now display whether the candidate passed the 
+examination or failed
+*/
+
+// int main()
+// {
+//     int h,e,s,si,ss;
+//     scanf("%d %d %d %d %d",&h,&e,&s,&si,&ss);
+//     if(h>33 && e>33 && s>33 && si>33 && ss>33)
+//     {
+//         printf("Pass all subject");
+//     }
+//     else {
+//         printf("failed ");
+//     }
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     int side1,side2,side3;
+//     printf("Enter sides: ");
+//     scanf("%d %d %d",&side1,&side2,&side3);
+//     if(side1+side2>side3 && side2+side3>side1 && side3+side1>side2)
+//     {
+//         printf("Valid triangle %d %d %d",side1,side2,side3);
+//     }
+//     else
+//     printf("Invalid triangle",side1,side2,side3);
+// }
+
+//Without using operator
+// int main()
+// {
+//     int n;
+//     printf("Enter number: ");
+//     scanf("%d",&n);
+
+//     if(n & 1)
+//         printf("odd");
+//     else
+//         printf("Even");
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     int n;
+//     printf("Enter number to check 3-digit number or not: ");
+//     scanf("%d",&n);
+//     if(n>=100 && n<=999)
+//         printf("3-digit number");
+//     else
+//         printf("not 3-digit number");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int num1;
+//     printf("Enter num1: ");
+//     scanf("%d",&num1);
+
+//     int num2;
+//     printf("Enter num2: ");
+//     scanf("%d",&num2);
+
+//     if(num1>num2)
+//     printf("num1 is %d",num1);
+
+//     else if(num2>num1)
+//     printf("num2 greater is %d",num2);
+//     else
+//     printf("Same number");
+//     return 0;
+// }
+
+
+int main()
+{
+    
+    return 0;
+}
